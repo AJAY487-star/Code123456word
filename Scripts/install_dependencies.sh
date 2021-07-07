771110e70db8e9212a311cab4119511785b02b02
AJAY487-star <64407115+AJAY487-star@users.noreply.github.com>
2021-07-07 13:20:41 +0530

    Update install_dependencies.sh

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -2,6 +2,4 @@
 
 apt install apache2 -y
 
-apt install php -y
-
-apt install php-mysql -y
+apt install php7.2-fpm php7.2-common php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y
